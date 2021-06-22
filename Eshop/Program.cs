2f01b11b2341c8accf8bea0bf6a38a8068b6ce24
Aliux <aliux02@gmail.com>
2021-06-22 13:03:11 +0300

    Add project files.

---
+++ Eshop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Eshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new Shop();
+
+            shop.ListItems();
+
+            shop.Buy("ItemName", 50);
+
+            shop.LoadItems();
+        }
+    }
+}
---
+++ Eshop/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Eshop
+{
+    internal class Shop
+    {
+        public Shop()
+        {
+
+        }
+
+        internal void ListItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Buy(string v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void LoadItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
