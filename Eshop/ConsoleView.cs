6be6eda05d844f0dd6df4c0ca93a8c37c6459e65
Aliux <aliux02@gmail.com>
2021-07-01 14:40:30 +0300

    Console.Writeline added to interface

---
+++ Eshop/ConsoleView.cs
@@ -0,0 +1,69 @@
+﻿using Eshop.Interfaces;
+using System;
+
+namespace Eshop
+{
+    internal class ConsoleView
+    {
+        private Shop shop;
+
+        private AiLogger _logger;
+
+        public ConsoleView(AiLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void View(Shop shop)
+        {
+            this.shop = shop;
+
+            var command = "";
+
+            while (command != "Exit")
+            {
+                try
+                {
+                    var EnteredText = InputLibrary.UserInputString();
+
+                    string[] ArrayOfEnteredText = EnteredText.Split(" ");
+
+                     switch (ArrayOfEnteredText[0].ToLower())
+                    {
+                        case "add":
+                            _logger.Write(Message.enterName);
+                            string name = InputLibrary.UserInputString().ToLower();
+                            _logger.Write(Message.enterPrice);
+                            decimal price = InputLibrary.UserInputToDecimal();
+                            _logger.Write(Message.enterQuantity);
+                            int quantity = InputLibrary.UserInputToInt(); 
+                            shop.LoadItems(name, price, quantity);
+                            break;
+                        case "buy":
+                            _logger.Write(Message.buyItemName);
+                            string itemName = Console.ReadLine().ToLower();
+                            _logger.Write(Message.buyItemQuantity);
+                            int buyQuantity = InputLibrary.UserInputToInt();
+                            shop.Buy(itemName, buyQuantity);
+                            break;
+                        case "list":
+                            shop.ListItems();
+                            break;
+                        case "exit":
+                            command = "Exit";
+                            break;
+                        default:
+                            throw new InvalidOperationException();
+                    }
+                }
+
+                catch (InvalidOperationException)
+                {
+                    _logger.Write("invalid command");
+                    _logger.Write(Message.enterCommand);
+                }
+
+            }
+        }
+    }
+}
--- Eshop/Program.cs
+++ Eshop/Program.cs
@@ -13,65 +13,15 @@ namespace Eshop
         static void Main(string[] args)
         {
             AiLogger logger = new ConsoleLogger();
-
-            var command = "";
             
             var shop = new Shop(logger);
 
             shop.WellcomeToShopMessage();
 
-            // bandyti visa while perkelti i shopa arba i kita klase
+            var view = new ConsoleView(logger);
 
-            while (command != "Exit")
-            {
-                try
-                {
-                    var EnteredText = InputLibrary.UserInputString();
+            view.View(shop);
 
-                    string[] ArrayOfEnteredText = EnteredText.Split(" ");
-
-                    
-
-                    switch (ArrayOfEnteredText[0].ToLower())
-                    {
-                        case "add":
-                            Console.Write(Message.enterName);
-                            string name = InputLibrary.UserInputString().ToLower();
-                            Console.Write(Message.enterPrice);
-                            decimal price = InputLibrary.UserInputToDecimal();
-                            Console.Write(Message.enterQuantity);
-                            int quantity = InputLibrary.UserInputToInt(); 
-                            shop.LoadItems(name, price, quantity);
-                            break;
-                        case "buy":
-                            Console.Write(Message.buyItemName);
-                            string itemName = Console.ReadLine().ToLower();
-                            Console.Write(Message.buyItemQuantity);
-                            int buyQuantity = InputLibrary.UserInputToInt();
-                            shop.Buy(itemName, buyQuantity);
-                            break;
-                        case "list":
-                            shop.ListItems();
-                            break;
-                        case "exit":
-                            command = "Exit";
-                            break;
-                        default:
-                            throw new InvalidOperationException();
-                    }
-                }
-
-                catch (InvalidOperationException)
-                {
-                    Console.WriteLine("invalid command");
-                    Console.WriteLine(Message.enterCommand);
-                }
-
-                //catch (Exception ex)
-                //{
-                //    Console.WriteLine($"Something went wrong {ex.Message}");
-                //}
-            }
         }
     }
 }
